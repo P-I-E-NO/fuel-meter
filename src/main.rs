a6cc44e5023b7d0d3c71074b5ce88aa09930be8c
Emiliano Maccaferri <inbox@emilianomaccaferri.com>
2023-11-28 23:47:13 +0100

    add: github actions

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ async fn main() {
     let port = var("HTTP_PORT").unwrap().parse::<u16>().unwrap();
     let router = web::build_app().await;
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
-    info!("{app} built and running on port {p}", app = var("APP_NAME").unwrap_or(var("CARGO_PKG_NAME").unwrap()), p = port);
+    info!("{app} built and running on port {p}", app = var("APP_NAME").unwrap_or("fuel-meter-svc".to_string()), p = port);
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, router).await.unwrap();
